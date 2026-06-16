149edd0da9a5de5f870e2a632c1a0d6d6950e1e7
unknown <Hari Venkatesh Pyla>
2026-06-16 15:31:24 +0530

    environment setup

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
+from dotenv import load_dotenv
 import os
 
-from dotenv import load_dotenv
-
 load_dotenv()
-
-
 def main():
     print("Hello from langchain-course!")
-
+    print(os.getenv("OPENAI_API_KEY"))
 if __name__ == "__main__":
     main()
+
+
